34c3164b2d36e73da17744e79dd616ad53f96e3b
prashant <ppo.opash@gmail.com>
2024-01-18 18:35:01 +0530

    Fixed edit research issue

--- src/app/layouts/auth-layout/pages/login/login.component.ts
+++ src/app/layouts/auth-layout/pages/login/login.component.ts
@@ -58,7 +58,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
       title: 'Legaltalk Tube login',
       url: `${environment.webUrl}login`,
       description: 'login page',
-      image: `${environment.webUrl}assets/images/landingpage/Healing-Tube-Logo.png`,
+      image: `${environment.webUrl}assets/images/landingpage/LegalTalk-Banner.jpg`,
     };
     // this.seoService.updateSeoMetaData(data);
   }
