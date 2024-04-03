17c804c08805d8ff062b1509cb1574e574090cd6
ppa-opash <ppa.opash@gmail.com>
2024-04-03 15:21:01 +0530

    Apply Chnages

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -82,6 +82,7 @@ import {
   faPhone,
   faLegal,
   faScaleBalanced,
+  faRefresh,
 } from '@fortawesome/free-solid-svg-icons';
 import { ClaimTokenModalComponent } from './modals/clai-1776-token-modal/claim-token-modal.component';
 import { WalletLinkComponent } from './modals/wallet-download-modal/1776-wallet.component';
@@ -225,7 +226,8 @@ export class SharedModule {
       faCircle,
       faPhone,
       faLegal,
-      faScaleBalanced
+      faScaleBalanced,
+      faRefresh
     );
   }
 }
