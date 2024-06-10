aa2a1294be9a6364c722d51c4da6765b22f7f663
prashant <ppo.opash@gmail.com>
2024-06-10 17:20:31 +0530

    Change state input to dropdown

--- src/app/layouts/main-layout/pages/communities/add-community-modal/add-community-modal.component.ts
+++ src/app/layouts/main-layout/pages/communities/add-community-modal/add-community-modal.component.ts
@@ -50,6 +50,8 @@ export class AddCommunityModalComponent implements OnInit, AfterViewInit {
   };
   allCountryData: any;
   defaultCountry = 'US';
+  allStateData: any;
+  selectedState = '';
 
   practitionerArea: any = [];
   practitionerEmphasis: any = [];
@@ -281,6 +283,7 @@ export class AddCommunityModalComponent implements OnInit, AfterViewInit {
         this.spinner.hide();
         this.allCountryData = result;
         this.communityForm.get('Zip').enable();
+        this.getAllState(this.defaultCountry)
       },
       error: (error) => {
         this.spinner.hide();
@@ -289,6 +292,24 @@ export class AddCommunityModalComponent implements OnInit, AfterViewInit {
     });
   }
 
+  onCountryChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.getAllState(target.value);
+  }
+  
+  getAllState(selectCountry) {
+    // this.spinner.show();
+    this.customerService.getStateData(selectCountry).subscribe({
+      next: (result) => {
+        this.spinner.hide();
+        this.allStateData = result;
+      },
+      error: (error) => {
+        this.spinner.hide();
+        console.log(error);
+      },
+    });
+  }
   changeCountry() {
     this.communityForm.get('Zip').setValue('');
     this.communityForm.get('State').setValue('');
